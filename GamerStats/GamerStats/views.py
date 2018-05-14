d11bbae9447000a5dee05e1c3f798a7ec8f932c8
Arun <arunkn28@gmail.com>
2018-05-14 22:40:54 +0530

    static changes in home page

--- GamerStats/GamerStats/views.py
+++ GamerStats/GamerStats/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
 from django.views.generic import View
-
+from django.shortcuts import render
 
 class BaseView(View):
     pass
 
 class HomePage(BaseView):
     def get(self,request):
-        return HttpResponse("<h1>Hey</h1>")
+        return render(request,'HomePage.html',{})
     
 
